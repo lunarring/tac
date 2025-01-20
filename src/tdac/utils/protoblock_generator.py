e103b305294e6347c56b9cde5d0d2826b4e2c466
Johannes Stelzer <jsdmail@gmail.com>
2025-01-20 16:56:39 +0000

    Refactor protoblock_generator.py to clarify task and test specifications
    - Added a new section for 'write_files' under the 'task' specification to ensure clarity on files that may need to be written.
    - Enhanced the 'test' specification to emphasize the need for a single, fulfillable test focused solely on new functionality, with detailed input data and expected outcomes.
    - Improved overall structure and readability of the protoblock generation instructions.

--- src/tdac/utils/protoblock_generator.py
+++ src/tdac/utils/protoblock_generator.py
@@ -53,13 +53,14 @@ seedblock:
 
 task:
   specification: given the entire codebase and the seedblock instruction, here we describe the task at hand very precisely. However we are not implementing the task here and we are not describing exactly HOW the code needs to be changed. You can come up with a proposal of how this could be achieved, but we do NOT need to implement it. Given your understanding of the seed block instructions and the codebase, you come up with a proposal for this!
-  write_files: list of files that may need to be written for the task. use relative file paths as given in the codebase. Don't list test files here.
 
 test: 
   specification: given the entire codebase and the seedblock instruction, here we describe the test specification for the task at hand. We are aiming to just write ONE single test, which is able to infer whether the functionality update in the main code has been implemented correctly or not. Thus, the goal is is figure out if the task has been implemented correctly. Critically, the test needs to be fulfillable. We do NOT need to test anything else than the NEW functionality given the task specification. The rest of the code will be tested by other means anyways, so don't mention it. However, if you are forseeing that the new test will clash with an existing test, because maybe code will be replaced, then mention it in the field 'replacements'.
   data: describe in detail the input data for the test and the expected outcome. Use the provided codebase as a reference. The more detail the better, make it as concrete as possible.
   replacements: list of tests that need to be replaced by the new test. use relative file paths as given in the codebase. leave empty if no replacements are needed.
 
+write_files: list of files that may need to be written for the task. use relative file paths as given in the codebase. Don't list test files here.
+  
 context_files: list of files that need to be read for context in order to implement the task and as background information for the test. use relative file paths as given in the codebase.
 
 --------------------
