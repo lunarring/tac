4bbe37685d777c2f5f4d4abb135cd3f20e1a9764
Johannes Stelzer <jsdmail@gmail.com>
2025-01-20 17:30:41 +0000

    Refactor seedblock_generator to enhance instruction clarity and test specifications
    - Updated the instruction format in the seedblock generation to emphasize concise language and direct orders.
    - Clarified the 'replacements' field in the test specification to indicate existing tests that need adaptation due to proposed changes.
    - Added a note to include replacements in the 'write_files' section, improving the overall structure and usability of the seedblock generation process.

--- src/tdac/utils/seedblock_generator.py
+++ src/tdac/utils/seedblock_generator.py
@@ -52,7 +52,7 @@ def generate_seedblock(directory: str, template_type: str = "default") -> str:
     template = f"""We have the following codebase: 
 {file_content}
     
-I want you to generate an instruction block (called seedblock) which is the input for a coding agent. The block has a very specific format that I need you to adhere to precisely, here is the format. For the text of each of the points, don't use line breaks, just write it all in one line.
+I want you to generate an instruction block (called seedblock) which is the input for a coding agent. The block has a very specific format that I need you to adhere to precisely, here is the format. For the text of each of the points, don't use line breaks, just write it all in one line. Write in very concise language, and write in a tone of giving direct and precise orders.
 
 --------------------
 
@@ -65,9 +65,9 @@ task:
 test: 
   specification: given the entire codebase and the seedblock instruction, here we describe the test specification for the task at hand. We are aiming to just write ONE single test, which is able to infer whether the functionality update in the main code has been implemented correctly or not. Thus, the goal is is figure out if the task has been implemented correctly. Critically, the test needs to be fulfillable. We do NOT need to test anything else than the NEW functionality given the task specification. The rest of the code will be tested by other means anyways, so don't mention it. However, if you are forseeing that the new test will clash with an existing test, because maybe code will be replaced, then mention it in the field 'replacements'.
   data: describe in detail the input data for the test and the expected outcome. Use the provided codebase as a reference. The more detail the better, make it as concrete as possible.
-  replacements: list of tests that need to be replaced by the new test. use relative file paths as given in the codebase. leave empty if no replacements are needed.
+  replacements: list of existing tests that need to be adapapted because of the proposed chances. use relative file paths as given in the codebase. leave empty if no replacements are needed.
 
-write_files: list of files that may need to be written for the task. use relative file paths as given in the codebase.
+write_files: list of files that may need to be written for the task. use relative file paths as given in the codebase. also add the replacements here!
   
 context_files: list of files that need to be read for context in order to implement the task and as background information for the test. use relative file paths as given in the codebase.
 
