0f5e6635fdb0a81158d110a99cc49c9804ca271c
Johannes Stelzer <jsdmail@gmail.com>
2025-01-28 10:09:34 +0000

    bad tests removed

--- tests/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="tdac",
-    version="0.1.0",
-    packages=find_packages(),
-    install_requires=[],
-    python_requires=">=3.8",
-)
