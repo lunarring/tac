bf7a8faaa1410a0907f77e38ef261b526ce0f8e6
Johannes Stelzer <jsdmail@gmail.com>
2025-02-28 10:53:00 +0000

    fixed test upd

--- tests/test_code_extractor.py
+++ tests/test_code_extractor.py
@@ -19,10 +19,12 @@ class TestCodeExtractor(unittest.TestCase):
         # foo: starts at 1, ends at 2 (if end_lineno available, should be 2)
         # bar: starts at 4, ends at 5
         # Baz: starts at 7, ends at 9
+        # Baz.__init__: starts at 8, ends at 9 (this is also included as a method)
         expected = [
             {'type': 'function', 'name': 'foo', 'start_line': 1, 'end_line': 2},
             {'type': 'function', 'name': 'bar', 'start_line': 4, 'end_line': 5},
-            {'type': 'class', 'name': 'Baz', 'start_line': 7, 'end_line': 9}
+            {'type': 'class', 'name': 'Baz', 'start_line': 7, 'end_line': 9},
+            {'type': 'method', 'name': 'Baz.__init__', 'start_line': 8, 'end_line': 9}
         ]
         
         # Since the 'end_line' might vary depending on how the AST computes it (especially with blank lines),
